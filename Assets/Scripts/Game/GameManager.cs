5db309eae903ad8375a57eed87b98c263206cfc2
Andrey Yarov <yarov@alarstudios.com>
2022-06-07 19:47:36 +0400

    Complete game manager

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using ScaredBug.UI;
 
-public class GameManager : MonoBehaviour
+namespace ScaredBug.Game
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameManager : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private UIManager m_UIManager;
+        [SerializeField] private LevelManager m_LevelManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private IEnumerator Start()
+        {
+            m_UIManager.SetFade(true, 0f);
+
+            while (true)
+            {
+                m_UIManager.HideLevelMenu();
+                m_UIManager.SetFade(false, 1f);
+                yield return m_LevelManager.Play();
+                m_UIManager.SetFade(true, 1f);
+                yield return new WaitForSeconds(1f);
+                bool waiting = true;
+                m_UIManager.ShowLevelMenu(() => waiting = false);
+                while (waiting)
+                    yield return null;
+            }
+        }
     }
 }
